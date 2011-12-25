b30300e495e4d01322464699bacea2788fdf156a
Olivier Louvignes <olivier@mg-crea.com>
2011-12-25 04:35:38 +0100

    Initial commit.

---
+++ extensions/helper/Assetic.php
@@ -0,0 +1,313 @@
+<?php
+/**
+ * Assetic Li3 Plugin
+ *
+ * @copyright     Copyright 2011, Olivier Louvignes (http://www.olouv.com)
+ * @license       http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace li3_assetic\extensions\helper;
+
+use lithium\core\Environment;
+use lithium\core\Libraries;
+use lithium\template\helper\Html;
+
+use Assetic\AssetWriter;
+use Assetic\AssetManager;
+use Assetic\FilterManager;
+use Assetic\Asset\AssetCollection;
+use Assetic\Asset\FileAsset;
+use Assetic\Asset\GlobAsset;
+
+//use Assetic\Asset\AssetCache;
+//use Assetic\Cache\FilesystemCache;
+
+/**
+ * A template helper that assists in generating CSS/JS static content
+ */
+class Assetic extends \lithium\template\Helper {
+
+	public static $config = array();
+	public static $filterManager;
+
+	protected $scriptAssetCollection;
+	protected $scriptAssetManager;
+	protected $scriptAssetWriter;
+	protected $scriptsWebroot;
+
+	protected $styleAssetCollection;
+	protected $styleAssetManager;
+	protected $styleAssetWriter;
+	protected $stylesWebroot;
+
+	/**
+	 * Configures this helper
+	 */
+	public static function config($config = array()) {
+
+		$defaults = array(
+			'optimize' => (Environment::get() == 'production'),
+			'debug' => (Environment::get() == 'development'),
+			'stylesPath' => LITHIUM_APP_PATH . DIRECTORY_SEPARATOR . 'webroot' . DIRECTORY_SEPARATOR . 'css',
+			'scriptsPath' => LITHIUM_APP_PATH . DIRECTORY_SEPARATOR . 'webroot' . DIRECTORY_SEPARATOR . 'js',
+			'filters' => array()
+		);
+		$config += $defaults;
+
+		// Merge config
+		static::$config = array_merge(static::$config, $config);
+
+		// Configure filters
+		static::registerFilters($config['filters']);
+
+	}
+
+	/**
+	 * Initializes helpers when used in a layout
+	 */
+	public function _init() {
+		parent::_init();
+
+		// Force helper static configuration
+		if(!static::$config) static::config();
+
+		$this->styleAssetCollection = new AssetCollection();
+		$this->styleAssetManager = new AssetManager();
+		// Initialize static assets writer
+		$this->styleAssetWriter = new AssetWriter(static::$config['stylesPath']);
+
+		$this->scriptAssetCollection = new AssetCollection();
+		$this->scriptAssetManager = new AssetManager();
+		// Initialize static assets writer
+		$this->scriptAssetWriter = new AssetWriter(static::$config['scriptsPath']);
+
+		// If we wanted to tap-into li3's generic helpers, that would be the way to achieve it using filters
+		/*$this->_context->helper('html')->applyFilter('style', function($self, $params, $chain) {
+			$params['path'] = $this->processParams($params['path'], $params['options']);
+			$result = $chain->next($self, $params, $chain);
+			return $result;
+		});*/
+
+	}
+
+	public function style($source, $options = array()) {
+
+		$defaults = array(
+			'type' => "style",
+			'target' => false,
+			'filters' => array()
+		);
+		$options += $defaults;
+
+		// Ensure arrays
+		if(!is_array($source)) $source = array($source);
+		if(!is_array($options['filters'])) $options['filters'] = array($options['filters']);
+
+		$ac =& $this->styleAssetCollection;
+		$aw =& $this->styleAssetWriter;
+
+		// Resolve filters
+		$filters = $this->resolveFilters($options['filters']);
+
+		foreach($source as $leaf) {
+			$leaf = self::guessExtension($leaf, $options);
+
+			if(strpos($leaf, '*')) $asset = new GlobAsset(APP_CSS_PATH . DS . $leaf, $filters);
+			else $asset = new FileAsset(APP_CSS_PATH . DS . $leaf, $filters);
+			$ac->add($asset);
+
+			if(!static::$config['optimize']) {
+				if((!empty($filters) || get_class($asset) == 'Assetic\Asset\GlobAsset')) {
+					$leaf = $options['target'] ?: self::normalizeExtension($leaf, $options['type']);
+					$asset->setTargetPath($leaf);
+					$aw->writeAsset($asset);
+				}
+				echo $this->_context->helper('html')->style($leaf);
+			}
+
+		}
+
+		return null;
+
+	}
+
+	public function styles($options = array()) {
+
+		if(!static::$config['optimize']) {
+			return $this->_context->styles();
+		}
+
+		$defaults = array(
+			'name' => "main",
+			'type' => "style",
+			'force' => false,
+			'filters' => array()
+		);
+		$options += $defaults;
+
+		// Ensure arrays
+		if(!is_array($options['filters'])) $options['filters'] = array($options['filters']);
+
+		$ac =& $this->styleAssetCollection;
+		$am =& $this->styleAssetManager;
+		$aw =& $this->styleAssetWriter;
+
+		// Resolve filters
+		$filters = $this->resolveFilters($options['filters']);
+		foreach($filters as $filter) {
+			$ac->ensureFilter($filter);
+		}
+
+		$ac->setTargetPath($options['name'] . '.css');
+		$am->set($options['name'], $ac);
+
+		//echo $ac->dump(); exit;
+		// Write static assets
+		if($options['force'] || !is_file($this->stylesWebroot . DS . $options['name'] . '.css')) {
+			$aw->writeManagerAssets($am);
+		}
+
+		return $this->_context->helper('html')->style($options['name'], array('inline' => true));
+
+	}
+
+	public function script($source, $options = array()) {
+
+		$defaults = array(
+			'type' => "script",
+			'target' => false,
+			'filters' => array()
+		);
+		$options += $defaults;
+
+		// Ensure arrays
+		if(!is_array($source)) $source = array($source);
+		if(!is_array($options['filters'])) $options['filters'] = array($options['filters']);
+
+		$ac =& $this->scriptAssetCollection;
+		$aw =& $this->scriptAssetWriter;
+
+		// Resolve filters
+		$filters = $this->resolveFilters($options['filters']);
+
+		foreach($source as $leaf) {
+			$leaf = self::guessExtension($leaf, $options);
+			if(strpos($leaf, '*')) $asset = new GlobAsset(APP_JS_PATH . DS . $leaf, $filters);
+			else $asset = new FileAsset(APP_JS_PATH . DS . $leaf, $filters);
+			$ac->add($asset);
+
+			if(!static::$config['optimize']) {
+				if((!empty($filters) || get_class($asset) == 'Assetic\Asset\GlobAsset')) {
+					$leaf = $options['target'] ?: self::normalizeExtension($leaf, $options['type']);
+					$asset->setTargetPath($leaf);
+					$aw->writeAsset($asset);
+				}
+				echo $this->_context->helper('html')->script($leaf);
+			}
+		}
+
+		return null;
+
+	}
+
+	public function scripts($options = array()) {
+
+		if(!static::$config['optimize']) {
+			return $this->_context->scripts();
+		}
+
+		$defaults = array(
+			'name' => "main",
+			'type' => "script",
+			'force' => false,
+			'filters' => array()
+		);
+		$options += $defaults;
+
+		// Ensure arrays
+		if(!is_array($options['filters'])) $options['filters'] = array($options['filters']);
+
+		$ac =& $this->scriptAssetCollection;
+		$am =& $this->scriptAssetManager;
+		$aw =& $this->scriptAssetWriter;
+
+		// Resolve filters
+		$filters = $this->resolveFilters($options['filters']);
+		foreach($filters as $filter) {
+			$ac->ensureFilter($filter);
+		}
+
+		$ac->setTargetPath($options['name'] . '.js');
+		$am->set($options['name'], $ac);
+
+		//echo $ac->dump(); exit;
+		// Write static assets
+		if($options['force'] || !is_file($this->scriptsWebroot . DS . $options['name'] . '.js')) {
+			$aw->writeManagerAssets($am);
+		}
+
+		return $this->_context->helper('html')->script($options['name'], array('inline' => true));
+
+	}
+
+	private function resolveFilters($filters) {
+
+		$fm =& static::$filterManager;
+
+		$resolvedFilters = array();
+		foreach($filters as $filter) {
+			if(!$fm || !$fm->has($filter)) throw new \BadRequestException(sprintf('Filter `%s` has not been configured.', $filter));
+			$resolvedFilters[] = $fm->get($filter);
+		}
+
+		return $resolvedFilters;
+
+	}
+
+	private function registerFilters($filters) {
+		if(!is_array($filters)) $filters = array($filters);
+
+		$fm =& static::$filterManager;
+		if(!$fm) $fm = new FilterManager();
+
+		foreach($filters as $key => $filter) {
+			static::$filterManager->set($key, $filter);
+		}
+
+	}
+
+	private static function guessExtension($leaf, $options = array()) {
+
+		if(empty($options['type'])) return $leaf;
+
+		if($options['type'] == 'style') {
+			if(preg_match('/(.css|.less|.sass|.scss)$/is', $leaf)) return $leaf;
+			else if(in_array('less', $options['filters']) || in_array('lessphp', $options['filters'])) $leaf .= '.less';
+			else if(in_array('sass', $options['filters'])) $leaf .= '.scss';
+			else $leaf .= '.css';
+		} else if($options['type'] == 'script') {
+			if(preg_match('/(.js)$/is', $leaf)) return $leaf;
+			$leaf .= '.js';
+		}
+
+		return $leaf;
+
+	}
+
+	private static function normalizeExtension($leaf, $type) {
+
+		if($type == 'style') {
+			$leaf = preg_replace('/([^\.]+)(.css|.less|.sass|.scss)?$/is', '$1.css', $leaf);
+		} else if($type == 'script') {
+			$leaf = preg_replace('/([^\.]+)(.js)?$/is', '$1.js', $leaf);
+		}
+
+		$leaf = str_replace('*', 'combined', $leaf);
+
+		return $leaf;
+
+	}
+
+}
+
+?>
---
+++ readme.md
@@ -0,0 +1,74 @@
+# Lithium Assetic Plugin #
+by `Olivier Louvignes`
+
+## DESCRIPTION ##
+
+This repository contains a [Lithium](https://github.com/UnionOfRAD/lithium) helper that wraps [Assetic](https://github.com/kriswallsmith/assetic) asset manager functionnality.
+
+## SETUP ##
+
+Using `Lithium Assetic Plugin` requires [Assetic](https://github.com/kriswallsmith/assetic).
+
+1. Clone [Assetic](https://github.com/kriswallsmith/assetic) to the `app/librairies/_source/assetic` folder.
+2. Symlink `app/librairies/Assetic` to `app/librairies/_source/assetic/src/Assetic`.
+2. Clone this plugin to `app/librairies/li3_assetic`
+3. Load both librairies in your `config/bootstrap/librairies.php` file :
+
+		Libraries::add('Assetic');
+		Libraries::add('li3_assetic');
+
+4. Configure the helper in your `config/bootstrap/media.php` file (or somewhere else), like :
+
+		/**
+		 * Assetic configuration
+		 */
+
+		use Assetic\Filter\LessphpFilter;
+		use Assetic\Filter\Yui;
+
+		Assetic::config(array(
+			'filters' => array(
+				'lessphp' => new LessphpFilter(),
+				'yui_css' => new Yui\CssCompressorFilter(LITHIUM_APP_PATH . '/../tools/yuicompressor-2.4.7.jar'),
+				'yui_js' => new Yui\JsCompressorFilter(LITHIUM_APP_PATH . '/../tools/yuicompressor-2.4.7.jar')
+			)
+		));
+
+5. Use the assetic helper in your layout :
+
+		// Regular call
+		<?php echo $this->assetic->script(array('libs/json2', 'libs/phonegap-1.2.0', 'libs/underscore', 'libs/mustache')); ?>
+		// Use some filter (will be processed even in development mode)
+		<?php echo $this->assetic->style(array('mobile/core'), array('filters' => array('lessphp'), 'target' => 'mobile.css')); ?>
+		// Use glob asset (will be processed even in development mode)
+		<?php echo $this->assetic->script(array('php/*.js'), array('target' => 'php.js'));
+
+6. Make sur to end your layout with final (production only by default) configuration :
+
+		<?php echo $this->assetic->styles(array('name' => 'mobile', 'filters' => 'yui_css')); ?>
+		<?php echo $this->assetic->scripts(array('name' => 'mobile', 'filters' => 'yui_js')); ?>
+
+7. You can activate compilation/filters with (like on top of your layout file) :
+
+		<?php $this->assetic->config(array('optimize' => true)); ?>
+
+
+## BUGS AND CONTRIBUTIONS ##
+
+Patches welcome! Send a pull request.
+
+Post issues on [Github](http://github.com/mgcrea/li3_assetic/issues)
+
+The latest code will always be [here](http://github.com/mgcrea/li3_assetic)
+
+## LICENSE ##
+
+Copyright 2011 Olivier Louvignes. All rights reserved.
+
+The MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
